082f6fc69df92b185acb4c78e394e269acba6a8e
domanskayelyzaveta <domanskaya_@ukr.net>
2024-05-20 15:32:41 +0200

    added header

---
+++ pratsia-lending/src/components/Header/HeaderNavdata.js
@@ -0,0 +1,8 @@
+export const navItems = [
+  { id: "home", label: "STARTSEITE" },
+  { id: "uberuns", label: "ÜBER UNS" },
+  { id: "unternehemen", label: "Für Unternehmen" },
+  { id: "arbeitnehmer", label: "Für Arbeitnehmer" },
+  { id: "INTERVIEWTERMIN", label: "interviewtermin" },
+  { id: "contacts", label: "KONTAKT" },
+];
