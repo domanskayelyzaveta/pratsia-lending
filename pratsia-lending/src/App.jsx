efd5d836aae6563295e6ec5b8184b3954b4fa7b7
domanskayelyzaveta <domanskaya_@ukr.net>
2024-05-21 13:51:04 +0200

    test build

--- pratsia-lending/src/App.jsx
+++ pratsia-lending/src/App.jsx
@@ -1,4 +1,4 @@
-// import Footer from "./components/Footer/Footer";
+// import Footer from "./components/Footer/Footer"; ..test
 import AboutUs from "./components/AboutUs/AboutUs";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
