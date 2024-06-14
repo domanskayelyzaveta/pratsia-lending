1782557d9c95184e5450e1424fa40d1003e74a40
domanskayelyzaveta <domanskaya_@ukr.net>
2024-06-14 15:03:40 +0200

    about us adaptation

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,35 +1,46 @@
-import photo from "../../images/hero/people2.jpg";
 import {
+  AboutSubTitle,
+  AboutUsDescText,
+  AboutUsDescriptionPartDiv,
+  AboutUsList,
   AboutUsSubTitle,
+  AboutUsText,
   AboutUsTitle,
   BtnListAboutUs,
+  HighlightedText,
   ImgAboutUs,
   ListItem,
   ListWrapper,
   TextAboutUs,
   TextAndPhotoDiv,
+  TitleWrapper,
+  WerdenSieDiv,
 } from "./AboutUs.styled";
+
+import photo from "../../images/hero/people2.jpg";
+import meeting from "../../images/works/meeting.png";
 
 import sprite from "../../assets/sprite.svg";
 import { Svg } from "../Header/Header.styled";
 import { useState } from "react";
-import List from "./List";
+import { BtnHero } from "../Hero/Hero.styled";
 
-const AboutUs = ({ title, content }) => {
+const AboutUs = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggleContent = () => {
-    setIsOpen(!isOpen);
+  const toggleContent = (index) => {
+    setIsOpen(isOpen === index ? null : index);
   };
+
   return (
     <section>
-      <div>
-        <AboutUsTitle> Über uns</AboutUsTitle>
+      <TitleWrapper>
+        <AboutUsTitle>Über uns</AboutUsTitle>
         <AboutUsSubTitle>
           Wir bauen langfristige und erfolgreiche Partnerschaften auf, die auf
           Vertrauen und Respekt basieren
         </AboutUsSubTitle>
-      </div>
+      </TitleWrapper>
       <div className="container">
         <TextAndPhotoDiv>
           <TextAboutUs>
@@ -42,44 +53,98 @@ const AboutUs = ({ title, content }) => {
         </TextAndPhotoDiv>
 
         <div>
-          <ul>
+          <AboutUsList>
             <ListItem>
-              <BtnListAboutUs onClick={toggleContent}>
+              <BtnListAboutUs onClick={() => toggleContent(0)}>
                 <ListWrapper>
                   <Svg>
                     <use href={`${sprite}#icon-chevron-bar`}></use>
                   </Svg>
-                  <p>{title}</p>
+                  <AboutUsText> Spezialisierung </AboutUsText>
                 </ListWrapper>
               </BtnListAboutUs>
-              {isOpen && (
-                <div className="content">
-                  <p>{content}</p>
+              {isOpen === 0 && (
+                <div>
+                  <AboutUsDescText>
+                    Wir sind spezialisiert auf die Vermittlung von talentierten
+                    Fachkräften in Branchen wie Medizin, Pflege, Sozialarbeit,
+                    Facility Management, Sanitär, Elektro und Automechanik / -
+                    mechatronik
+                  </AboutUsDescText>
                 </div>
               )}
             </ListItem>
             <ListItem>
-              <BtnListAboutUs onClick={toggleContent}>
+              <BtnListAboutUs onClick={() => toggleContent(1)}>
                 <ListWrapper>
                   <Svg>
                     <use href={`${sprite}#icon-chevron-bar`}></use>
                   </Svg>
-                  <p> Ein integrierter Ansatz </p>
+                  <AboutUsText>Ein integrierter Ansatz</AboutUsText>
                 </ListWrapper>
               </BtnListAboutUs>
+              {isOpen === 1 && (
+                <div>
+                  <AboutUsDescText>
+                    Unser ganzheitlicher Ansatz umfasst nicht nur die
+                    Personalvermittlung, sondern auch zusätzliche
+                    Dienstleistungen wie vorbereitende und vertiefende
+                    Deutschkurse und Unterstützung bei der Anerkennung von
+                    Qualifikationen der jeweiligen Branchen
+                  </AboutUsDescText>
+                </div>
+              )}
             </ListItem>
             <ListItem>
-              <BtnListAboutUs onClick={toggleContent}>
+              <BtnListAboutUs onClick={() => toggleContent(2)}>
                 <ListWrapper>
                   <Svg>
                     <use href={`${sprite}#icon-chevron-bar`}></use>
                   </Svg>
-                  <p> Fundierte Arbeit </p>
+                  <AboutUsText>Fundierte Arbeit</AboutUsText>
                 </ListWrapper>
               </BtnListAboutUs>
+              {isOpen === 2 && (
+                <div>
+                  <AboutUsDescText>
+                    Fundierte Arbeit Unsere Arbeit basiert auf ethischen
+                    Geschäftspraktiken und Transparenz in der Kommunikation.
+                    Pratsia kann Ihnen helfen, die besten Talente für Ihr
+                    Unternehmen zu gewinnen und Ihre Einstellungsziele zu
+                    erreichen. Wir helfen Ihnen, eine reibungslose Integration
+                    in den deutschen Arbeitsmarkt zu gewährleisten
+                  </AboutUsDescText>
+                </div>
+              )}
             </ListItem>
-          </ul>
+          </AboutUsList>
         </div>
+
+        <AboutUsDescriptionPartDiv>
+          <AboutUsDescText>
+            Unsere langjährige Erfahrung, unser umfassendes Branchenwissen und
+            unser internationales Netzwerk ermöglichen es uns, maßgeschneiderte
+            Rekrutierungslösungen anzubieten, die den Bedürfnissen unserer
+            Kunden entsprechen.
+          </AboutUsDescText>
+
+          <TextAndPhotoDiv>
+            <ImgAboutUs src={meeting}></ImgAboutUs>
+            <WerdenSieDiv>
+              <AboutSubTitle>Werden Sie Teil des Teams!</AboutSubTitle>
+              <AboutUsDescText>
+                Auf der Suche nach neuen beruflichen Herausforderungen? Unser
+                Personalvermittlungsunternehmen bietet spannende
+                Karrieremöglichkeiten in verschiedenen Branchen.
+              </AboutUsDescText>
+            </WerdenSieDiv>
+          </TextAndPhotoDiv>
+
+          <HighlightedText>
+            Bewerben Sie sich jetzt und entdecken Sie Ihre Zukunft mit uns!
+          </HighlightedText>
+          <BtnHero>Interviewtermin</BtnHero>
+        </AboutUsDescriptionPartDiv>
       </div>
     </section>
   );
--- src/components/AboutUs/AboutUs.styled.js
+++ src/components/AboutUs/AboutUs.styled.js
@@ -1,5 +1,16 @@
 import styled from "styled-components";
 
+export const TitleWrapper = styled.div`
+  margin-bottom: 20px;
+
+  @media screen and (min-width: 768px) {
+    margin-bottom: 40px;
+  }
+  @media screen and (min-width: 1440px) {
+    margin-bottom: 56px;
+  }
+`;
+
 export const AboutUsTitle = styled.h2`
   padding: 32px 20px 16px 20px;
 
@@ -22,25 +33,26 @@ export const AboutUsSubTitle = styled.p`
   font-weight: 400;
   line-height: 150%;
 
-  margin-bottom: 20px;
-
   color: var(--blue-dark);
   background: var(--beige-200);
 
   @media screen and (min-width: 1440px) {
     padding: 56px 84px;
-    margin-bottom: 56px;
 
     font-size: 24px;
   }
 `;
 
 export const TextAndPhotoDiv = styled.div`
-  @media screen and (min-width: 1440px) {
+  @media screen and (min-width: 768px) {
     display: flex;
-    flex-direction: row;
+    flex-direction: row-reverse;
     align-items: center;
     gap: 24px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    flex-direction: row;
   }
 `;
 
@@ -51,15 +63,29 @@ export const TextAboutUs = styled.p`
 
   margin-bottom: 20px;
 
+  @media screen and (min-width: 768px) {
+    font-size: 20px;
+  }
+
   @media screen and (min-width: 1440px) {
     width: 624px;
-
-    font-size: 20px;
     margin-bottom: 0px;
   }
 `;
 
 export const ImgAboutUs = styled.img`
+  height: 200px;
+  object-fit: cover;
+  margin-bottom: 20px;
+
+  border-radius: 8px;
+
+  @media screen and (min-width: 768px) {
+    width: 348px;
+    height: 270px;
+
+    border-radius: 16px;
+  }
   @media screen and (min-width: 1440px) {
     width: 624px;
     height: 320px;
@@ -83,6 +109,55 @@ export const ListWrapper = styled.div`
   display: flex;
 `;
 
+export const AboutUsList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+
+  margin-bottom: 32px;
+`;
+
 export const ListItem = styled.li`
   //   display: flex;
+`;
+
+export const AboutUsText = styled.p`
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 150%;
+  color: var(--black-200);
+`;
+
+export const AboutUsDescText = styled.p`
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 150%;
+
+  color: var(--black-200);
+`;
+
+export const AboutSubTitle = styled.h3`
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 150%;
+  color: var(--blue-dark);
+`;
+
+export const HighlightedText = styled.p`
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 150%;
+  color: var(--blue-300);
+`;
+
+export const AboutUsDescriptionPartDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+export const WerdenSieDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
 `;
