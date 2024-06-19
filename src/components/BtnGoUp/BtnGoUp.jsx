601c01ff72190cb43ec2e0abe0cb735e5e6e0a0c
Aleksandr2905 <natalukha@ukr.net>
2024-06-19 20:57:17 +0300

    added component BtnGoUp

---
+++ src/components/BtnGoUp/BtnGoUp.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import sprite from "../../assets/sprite.svg";
+import { Button } from "./BtnGoUp.styled";
+
+const BtnGoUp = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      setIsVisible(scrollY > 100);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <>
+      {isVisible && (
+        <Button onClick={scrollToTop} className="container">
+          <svg width="32" height="32">
+            <use href={`${sprite}#icon-arrow-up`} />
+          </svg>
+        </Button>
+      )}
+    </>
+  );
+};
+
+export default BtnGoUp;
