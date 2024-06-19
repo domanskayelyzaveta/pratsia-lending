63bf50d57a0c8fc9226275489c201065e4c07bd5
Aleksandr2905 <natalukha@ukr.net>
2024-06-19 18:20:37 +0300

    added style tablet forma

--- src/components/AppointmentForm/AppointmentForm.styled.js
+++ src/components/AppointmentForm/AppointmentForm.styled.js
@@ -2,30 +2,47 @@ import DatePicker from "react-datepicker";
 import styled from "styled-components";
 
 export const SectionAppointmentForm = styled.section`
-  padding-top: 32px;
+  padding-top: 20px;
+
+  @media screen and (min-width: 768px) {
+    padding-top: 40px;
+  }
 `;
 
 export const TitlesWrapper = styled.section`
   display: flex;
   flex-direction: column;
-  align-items: center;
   gap: 8px;
+
+  @media screen and (min-width: 768px) {
+    gap: 12px;
+  }
 `;
 
 export const TitleAppointmentForm = styled.h2`
+  text-align: center;
   font-size: 16px;
   font-weight: 400;
   line-height: 150%;
 
   color: var(--blue-dark);
+
+  @media screen and (min-width: 768px) {
+    font-size: 24px;
+  }
 `;
 
 export const SubTitleAppointmentForm = styled.p`
+  text-align: center;
   font-size: 14px;
   font-weight: 400;
   line-height: 150%;
-
   margin-bottom: 20px;
+
+  @media screen and (min-width: 768px) {
+    font-size: 20px;
+    margin-bottom: 24px;
+  }
 `;
 
 export const Form = styled.form`
@@ -33,22 +50,41 @@ export const Form = styled.form`
   flex-direction: column;
   align-items: center;
   gap: 15px;
+  width: 280px;
+  margin: auto;
+
+  @media screen and (min-width: 768px) {
+    width: 534px;
+    gap: 20px;
+  }
 `;
 
 export const Label = styled.label`
   display: flex;
   flex-direction: row;
   gap: 2px;
+  margin-bottom: 4px;
 
   font-size: 11px;
   font-weight: 400;
   line-height: 150%;
+
+  & :nth-child(1) {
+    margin-top: 2px;
+  }
+
+  @media screen and (min-width: 768px) {
+    font-size: 16px;
+
+    & :nth-child(1) {
+      margin-top: 4px;
+    }
+  }
 `;
 
 export const InputWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 4px;
   position: relative;
 `;
 
@@ -86,7 +122,19 @@ export const Input = styled.input`
       ${(props) => (props.$errors ? "var(--red)" : "var(--blue-300)")};
     outline: none;
   }
+
+  @media screen and (min-width: 768px) {
+    width: 534px;
+    height: 52px;
+    font-size: 18px;
+    border-radius: 12px;
+
+    &::placeholder {
+      font-size: 18px;
+    }
+  }
 `;
+
 export const TextArea = styled.textarea`
   font-size: 12px;
   font-weight: 400;
@@ -119,6 +167,17 @@ export const TextArea = styled.textarea`
       ${(props) => (props.$errors ? "var(--red)" : "var(--blue-300)")};
     outline: none;
   }
+
+  @media screen and (min-width: 768px) {
+    width: 534px;
+    height: 80px;
+    font-size: 18px;
+    border-radius: 12px;
+
+    &::placeholder {
+      font-size: 18px;
+    }
+  }
 `;
 
 export const TextError = styled.p`
@@ -131,28 +190,43 @@ export const TextError = styled.p`
   font-weight: 400;
   line-height: 150%;
   color: var(--red);
+
+  @media screen and (min-width: 768px) {
+    font-size: 14px;
+    bottom: -18px;
+  }
 `;
 
 export const BtnForm = styled.button`
-  margin-top: 32px;
+  margin-top: 17px;
   text-align: center;
   font-size: 14px;
   font-weight: 500;
   line-height: 150%;
   color: var(--blue-200);
 
-  width: 280px;
+  width: 100%;
   height: 40px;
   padding: 8px;
 
   border-radius: 8px;
   border: 1px solid var(--blue-200);
   background: var(--beige-100);
+
+  &:hover {
+    border: 3px solid var(--blue-200);
+    padding: 6px;
+  }
+
+  @media screen and (min-width: 768px) {
+    margin-top: 28px;
+    font-size: 20px;
+    height: 52px;
+  }
 `;
 
 export const StyledDatePicker = styled(DatePicker)`
   width: 280px;
-  height: 100%;
   border: none;
   outline: none;
   padding: 8px 16px;
@@ -180,5 +254,12 @@ export const StyledDatePicker = styled(DatePicker)`
   .react-datepicker__day--keyboard-selected {
     background-color: var(--blue-dark);
     color: var(--white);
+  }
+
+  @media screen and (min-width: 768px) {
+    width: 534px;
+    font-size: 18px;
+    height: 52px;
+    border-radius: 12px;
   }
 `;
