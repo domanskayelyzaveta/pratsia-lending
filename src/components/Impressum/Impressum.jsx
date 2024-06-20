2c12a7e5e483f45be5b3a36a19a17aea8e3a7a7e
domanskayelyzaveta <domanskaya_@ukr.net>
2024-06-20 14:42:23 +0200

    impressum and daten added

--- src/components/Impressum/Impressum.jsx
+++ src/components/Impressum/Impressum.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 import sprite from "../../assets/sprite.svg";
 import { Svg } from "../Header/Header.styled";
+
 import {
   BtnImpress,
   BtnTextImpress,
+  ImpressWrapper,
   TextImpress,
   TextImpressWrapper,
   TitleImpress,
 } from "./Impressum.styled";
+import { useNavigate } from "react-router-dom";
 
 const Impressum = () => {
+  const navigate = useNavigate();
+
+  const handleBackClick = () => {
+    navigate(-1);
+  };
+
   return (
-    <div className="container">
-      <BtnImpress>
+    <ImpressWrapper className="container">
+      <BtnImpress onClick={handleBackClick}>
         <Svg>
           <use href={`${sprite}#icon-arrow-left-short`}></use>
         </Svg>
@@ -21,22 +30,33 @@ const Impressum = () => {
       <TitleImpress>Impressum</TitleImpress>
 
       <TextImpressWrapper>
-        <TextImpress>
-          Pratsia UG (haftungsbeschränkt) Garnstraße 12 47798 Krefeld
-        </TextImpress>
+        <div>
+          <TextImpress>Pratsia UG (haftungsbeschränkt)</TextImpress>
+          <TextImpress>Garnstraße 12</TextImpress>
+          <TextImpress>47798 Krefeld</TextImpress>
+        </div>
 
-        <TextImpress>
-          Handelsregister: HRB Registergericht: Amtsgericht Krefeld
-        </TextImpress>
-        <TextImpress>
-          Vertreten durch: Mykola Sytnyk Kontakt Telefon: 0170 1481355 E-Mail:
-          info@pratsia.de EU-Streitschlichtung Die Europäische Kommission stellt
-          eine Plattform zur Online-Streitbeilegung (OS) bereit:
-          https://ec.europa.eu/consumers/odr/. Unsere E-Mail-Adresse finden Sie
-          oben im Impressum. Quelle: eRecht24
-        </TextImpress>
+        <div>
+          <TextImpress>Handelsregister: HRB</TextImpress>
+          <TextImpress>Registergericht: Amtsgericht Krefeld</TextImpress>
+        </div>
+
+        <div>
+          <TextImpress>Vertreten durch:</TextImpress>
+          <TextImpress>Mykola Sytnyk</TextImpress>
+          <TextImpress>Kontakt</TextImpress>
+          <TextImpress>Telefon: 0170 1481355</TextImpress>
+          <TextImpress>E-Mail: info@pratsia.de</TextImpress>
+          <TextImpress>EU-Streitschlichtung</TextImpress>
+          <TextImpress>
+            Die Europäische Kommission stellt eine Plattform zur
+            Online-Streitbeilegung (OS) bereit:
+          </TextImpress>
+          <TextImpress>https://ec.europa.eu/consumers/odr/.</TextImpress>
+          <TextImpress>Quelle: eRecht24</TextImpress>
+        </div>
       </TextImpressWrapper>
-    </div>
+    </ImpressWrapper>
   );
 };
 
--- src/components/Impressum/Impressum.styled.js
+++ src/components/Impressum/Impressum.styled.js
@@ -1,7 +1,20 @@
 import styled from "styled-components";
 
+export const ImpressWrapper = styled.div`
+  // margin-top: 24px;
+
+  // @media screen and (min-width: 768px) {
+  //   margin-top: 82px;
+  // }
+
+  // @media screen and (min-width: 1440px) {
+  //   margin-top: 118px;
+  }
+`;
+
 export const BtnImpress = styled.button`
   display: flex;
+  align-items: center;
   gap: 4px;
 
   margin-bottom: 20px;
@@ -21,6 +34,10 @@ export const BtnTextImpress = styled.p`
   line-height: 150%;
 
   color: var(--blue-200);
+
+  @media screen and (min-width: 768px) {
+    font-size: 20px;
+  }
 `;
 
 export const TitleImpress = styled.h2`
@@ -31,6 +48,14 @@ export const TitleImpress = styled.h2`
   line-height: 150%;
 
   color: var(--blue-300);
+
+  @media screen and (min-width: 768px) {
+    font-size: 28px;
+  }
+
+  @media screen and (min-width: 768px) {
+    font-size: 32px;
+  }
 `;
 
 export const TextImpressWrapper = styled.div`
