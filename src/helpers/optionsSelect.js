f6c4d30c52fe16324e29b720fe00e4c17393cca1
domanskayelyzaveta <domanskaya_@ukr.net>
2024-06-24 14:42:28 +0200

    translation to form added

--- src/helpers/optionsSelect.js
+++ src/helpers/optionsSelect.js
@@ -1,9 +1,11 @@
-export const options = [
-  { value: "Herr", label: "Herr" },
-  { value: "Frau", label: "Frau" },
+import { t } from "i18next";
+
+export const options = () => [
+  { value: "Herr", label: t("select1H") },
+  { value: "Frau", label: t("select1F") },
 ];
 
-export const optionsTime = [
-  { value: "Nachmittag", label: "Nachmittag" },
-  { value: "Vormittag", label: "Vormittag" },
+export const optionsTime = () => [
+  { value: "Nachmittag", label: t("select3N") },
+  { value: "Vormittag", label: t("select3V") },
 ];
