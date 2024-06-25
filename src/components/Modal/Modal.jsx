307e455929d2f8dda9d73ba5c65d155fc46ed331
Aleksandr2905 <natalukha@ukr.net>
2024-06-25 13:18:08 +0300

    added animation modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,33 +1,82 @@
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import sprite from "../../assets/sprite.svg";
 import { Backdrop, Button, Content } from "./Modal.styled";
 import { createPortal } from "react-dom";
+import gsap from "gsap";
+
+const usePrevious = (value) => {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  }, [value]);
+  return ref.current;
+};
 
 const Modal = ({ show, handleClose, children }) => {
   const modal = document.getElementById("modal");
+  const contentRef = useRef(null);
+  const prevShow = usePrevious(show);
+  const [isClosing, setIsClosing] = useState(false);
 
   useEffect(() => {
     const handleEscape = (event) => {
       if (show && event.key === "Escape") {
-        handleClose();
+        startClosing();
       }
     };
+
     document.body.style.overflow = show ? "hidden" : "auto";
     window.addEventListener("keydown", handleEscape);
+
+    if (prevShow !== show) {
+      if (show && contentRef.current) {
+        gsap.fromTo(
+          contentRef.current,
+          { scale: 0, opacity: 0 },
+          { scale: 1, opacity: 1, duration: 1, ease: "back.out(1.1)" }
+        );
+      } else if (contentRef.current && !show) {
+        gsap.to(contentRef.current, {
+          scale: 0,
+          opacity: 0,
+          duration: 1,
+          ease: "back.in(1.1)",
+          onComplete: () => {
+            setIsClosing(false);
+            handleClose();
+          },
+        });
+      }
+    }
+
     return () => {
       window.removeEventListener("keydown", handleEscape);
       document.body.style.overflow = "auto";
     };
-  }, [show]);
+  }, [show, handleClose, prevShow]);
 
-  if (!show || modal === null) {
+  const startClosing = () => {
+    setIsClosing(true);
+    gsap.to(contentRef.current, {
+      scale: 0,
+      opacity: 0,
+      duration: 1,
+      ease: "back.in(1.1)",
+      onComplete: () => {
+        setIsClosing(false);
+        handleClose();
+      },
+    });
+  };
+
+  if (!show && !isClosing) {
     return null;
   }
 
   return createPortal(
-    <Backdrop onClick={handleClose}>
-      <Content onClick={(e) => e.stopPropagation()}>
-        <Button onClick={handleClose}>
+    <Backdrop onClick={startClosing}>
+      <Content ref={contentRef} onClick={(e) => e.stopPropagation()}>
+        <Button onClick={startClosing}>
           <svg width={24} height={24}>
             <use href={`${sprite}#icon-close`} />
           </svg>
