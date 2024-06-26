5f86a0fb9a39f1c18de403a1eb0b4effb2c9868f
domanskayelyzaveta <domanskaya_@ukr.net>
2024-06-26 16:10:40 +0200

    formField fixed

--- src/components/AppointmentForm/FormField.jsx
+++ src/components/AppointmentForm/FormField.jsx
@@ -1,3 +1,48 @@
+// import React from "react";
+// import "react-datepicker/dist/react-datepicker.css";
+
+// import {
+//   Input,
+//   InputWrapper,
+//   Label,
+//   TextError,
+// } from "./AppointmentForm.styled";
+// import Star from "./Star";
+
+// const FormField = ({
+//   label,
+//   type,
+//   name,
+//   register,
+//   errors,
+//   required,
+//   placeholder,
+//   component,
+//   id,
+// }) => (
+//   <InputWrapper>
+//     <Label htmlFor={id}>
+//       {label}
+//       {required && <Star />}
+//     </Label>
+//     {component ? (
+//       component
+//     ) : (
+//       <Input
+//         type={type}
+//         name={name}
+//         id={name}
+//         {...register(name, { required })}
+//         $errors={errors[name]}
+//         placeholder={placeholder}
+//       />
+//     )}
+//     <TextError>{errors[name]?.message}</TextError>
+//   </InputWrapper>
+// );
+
+// export default FormField;
+
 import React from "react";
 import "react-datepicker/dist/react-datepicker.css";
 
@@ -18,10 +63,9 @@ const FormField = ({
   required,
   placeholder,
   component,
-  id,
 }) => (
   <InputWrapper>
-    <Label htmlFor={id}>
+    <Label htmlFor={name}>
       {label}
       {required && <Star />}
     </Label>
