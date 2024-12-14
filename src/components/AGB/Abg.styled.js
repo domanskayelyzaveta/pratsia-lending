13ee0af0fc7b054ff9812ddf931c17752ad351b1
domanskayelyzaveta <domanskaya_@ukr.net>
2024-12-14 07:40:50 +0100

    added styles and components optimization

--- src/components/AGB/Abg.styled.js
+++ src/components/AGB/Abg.styled.js
@@ -1,5 +1,17 @@
 import styled from "styled-components";
 
+export const AgbWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+export const ItemsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
 export const TextAbg = styled.p`
   font-size: 14px;
   font-weight: 400;
@@ -10,5 +22,35 @@ export const TextAbg = styled.p`
   @media screen and (min-width: 768px) {
     font-size: 20px;
     margin-bottom: 0px;
+  }
+`;
+
+export const ListAgb = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 6px;
+`;
+
+export const ListItemAgb = styled.li`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 150%;
+  position: relative;
+  padding-left: 12px;
+
+  &::before {
+    content: "";
+    position: absolute;
+    left: 0;
+    top: 12px;
+    transform: translateY(-50%);
+    width: 3px;
+    height: 3px;
+    background-color: var(--blue-300);
+    border-radius: 50%;
   }
 `;
---
+++ src/components/AGB/dataAgb.js
@@ -0,0 +1,166 @@
+export const agbData = {
+  title: "Allgemeine Geschäftsbedingungen Pratsia UG (haftungsbeschränkt)",
+  date: "(Stand: 1.07.2024)",
+  sections: [
+    {
+      title: "1. Geltungsbereich",
+      content: [
+        {
+          text: `Diese Allgemeinen Geschäftsbedingungen (AGB) gelten für alle
+                    Dienstleistungen, die von Pratsia UG (haftungsbeschränkt) (im
+                    Folgenden „Agentur“ genannt) gegenüber Arbeitssuchenden (im Folgenden
+                    „Bewerber“ genannt) und möglichen Arbeitgebern (im Folgenden
+                    „Partner“) erbracht werden. Mit der Nutzung der Dienstleistungen der
+                    Agentur erkennt der Kunde die AGB in der jeweils gültigen Fassung an.`,
+        },
+      ],
+    },
+    {
+      title: "2. Vertragsgegenstand",
+      content: [
+        {
+          text: "Die Agentur unterstützt den Bewerber als auch den Partner bei:",
+          list: [
+            "der Arbeitsvermittlung für medizinische Fachkräfte, einschließlich Pflegepersonal, Ärzten und Betreuungskräften.",
+            "der Beratung zur Anerkennung von Berufsabschlüssen.",
+            "der Unterstützung bei der Erstellung und Einreichung erforderlicher Unterlagen.",
+          ],
+        },
+        {
+          text: "Die Agentur verpflichtet sich, dem Kunden mindestens eine geeignete Arbeitsstelle gemäß den Vorgaben des Kunden vorzuschlagen. Die Agentur übernimmt keine Verantwortung für die Bedingungen der Arbeitsverträge, die zwischen dem Kunden und den potenziellen Arbeitgebern abgeschlossen werden.",
+        },
+      ],
+    },
+    {
+      title: "3. Leistungen der Agentur",
+      content: [
+        {
+          text: "Die Dienstleistungen der Agentur umfassen insbesondere:",
+          list: [
+            "Recherche und Vermittlung von Arbeitsplätzen.",
+            "Bereitstellung von Informationen und Dokumentation, die für die Bewerbung erforderlich sind.",
+            "Kommunikation mit potenziellen Arbeitgebern im Namen des Kunden.",
+          ],
+        },
+        {
+          text: "Die Agentur garantiert keinen Erfolg bei der Vermittlung einer Arbeitsstelle oder bei der Anerkennung von Berufsabschlüssen. Der Kunde trägt die Verantwortung dafür, die Bedingungen eines Arbeitsvertrages sorgfältig zu prüfen, bevor er diesen unterzeichnet.",
+        },
+      ],
+    },
+    //========= 4 ===========
+    {
+      title: "4. Pflichten des Kunden und Pflichten der Agentur",
+      content: [
+        {
+          text: "Der Kunde verpflichtet sich, der Agentur alle notwendigen Informationen und Dokumente wahrheitsgemäß und vollständig zur Verfügung zu stellen. Dazu gehören insbesondere:",
+          list: [
+            "Persönliche Daten (z. B. Name, Adresse, Kontaktinformationen).",
+            "Nachweise über Qualifikationen und Berufsabschlüsse.",
+            "Präferenzen für die gewünschte Arbeitsstelle (z. B. Region, Position, Gehalt).",
+          ],
+        },
+        {
+          text: "Änderungen dieser Angaben sind der Agentur unverzüglich mitzuteilen. Die Agentur verpflichtet sich, dem Partner alle notwendigen Informationen und Dokumente wahrheitsgemäß und vollständig zur Verfügung zu stellen. Dazu gehören insbesondere:",
+          list: [
+            "Persönliche Daten (z. B. Name, Adresse, Kontaktinformationen).",
+            "Nachweise über Qualifikationen und Berufsabschlüsse.",
+            "Präferenzen für die gewünschte Arbeitsstelle (z. B. Region, Position, Gehalt).",
+          ],
+        },
+      ],
+      footer: `Die Agentur garantiert keinen Erfolg bei der Vermittlung einer
+           Arbeitsstelle oder bei der Anerkennung von Berufsabschlüssen. Der
+           Kunde trägt die Verantwortung dafür, die Bedingungen eines
+           Arbeitsvertrages sorgfältig zu prüfen, bevor er diesen unterzeichnet.`,
+    },
+    //========= 5 ===========
+    {
+      title: "5. Kosten und Vergütung",
+      content: [
+        {
+          text: `Alle Dienstleistungen der Agentur für den Arbeitssuchenden sind für
+             den Kunden kostenlos. Die Kosten für die Übersetzung von Dokumenten
+             sowie für Gebühren und Kommissionen während des Anerkennungsverfahrens
+             trägt die Agentur.`,
+        },
+      ],
+    },
+    //========= 6 ===========
+    {
+      title: "6. Datenschutz (DSGVO)",
+      content: [
+        {
+          text: `Die Agentur verpflichtet sich, die personenbezogenen Daten des Kunden
+                 gemäß der Datenschutz-Grundverordnung (DSGVO) zu schützen. Die Daten
+                 werden ausschließlich für die Arbeitsvermittlung und damit verbundene
+                 Dienstleistungen verwendet. Eine Weitergabe an Dritte erfolgt nur,
+                 soweit dies für die Vermittlung erforderlich ist (z. B. an potenzielle
+                 Arbeitgeber). Der Arbeitssuchende kann jederzeit:`,
+          list: [
+            "Auskunft über die Verarbeitung seiner Daten verlangen.",
+            "die Berichtigung oder Löschung seiner Daten beantragen.",
+            "die erteilte Einwilligung zur Datenverarbeitung widerrufen.",
+          ],
+        },
+        {
+          text: `Anfragen zur Datenverarbeitung können schriftlich per E-Mail an
+                 info@pratsia.de gerichtet werden.`,
+        },
+      ],
+    },
+    //========= 7 ===========
+    {
+      title: "7. Haftungsausschluss",
+      content: [
+        {
+          text: "Die Agentur haftet nicht für:",
+          list: [
+            "Entscheidungen der Partner und der Bewerber, einschließlich Absagen oder Vertragsbedingungen.",
+            "Verzögerungen oder Ablehnungen bei der Anerkennung von Berufsabschlüssen.",
+            "Kosten oder finanzielle Verluste, die dem Kunden aufgrund von Absagen oder unzureichenden Vertragsbedingungen entstehen",
+            "Ereignisse höherer Gewalt (z. B. Naturkatastrophen, technische Störungen, Streiks, politische Unruhen).",
+          ],
+        },
+        {
+          text: "Die Agentur haftet ausschließlich für Schäden, die auf vorsätzliches  oder grob fahrlässiges Verhalten zurückzuführen sind.",
+        },
+      ],
+    },
+    //========= 8 ===========
+    {
+      title: "8. Vertragskündigung",
+      content: [
+        {
+          text: "Der Vertrag zwischen der Agentur und dem Kunden kann von beiden Parteien schriftlich (per E-Mail oder Post) gekündigt werden.",
+        },
+      ],
+    },
+    //========= 9 ===========
+    {
+      title: "9. Änderungen der AGB",
+      content: [
+        {
+          text: "Die Agentur behält sich das Recht vor, diese AGB jederzeit zu ändern. Änderungen werden dem Kunden schriftlich oder per E-Mail mitgeteilt. Der Kunde als auch der Partner haben 14 Tage Zeit, den Änderungen zu  widersprechen. Erfolgt kein Widerspruch innerhalb dieser Frist, gelten die Änderungen als akzeptiert.",
+        },
+      ],
+    },
+    //========= 10 ===========
+    {
+      title: "10. Anwendbares Recht und Gerichtsstand",
+      content: [
+        {
+          text: "Für alle Rechtsbeziehungen zwischen der Agentur und dem Kunden gilt deutsches Recht. Gerichtsstand ist Krefeld, Deutschland.",
+        },
+      ],
+    },
+    //========= 11 ===========
+    {
+      title: "11. Salvatorische Klausel",
+      content: [
+        {
+          text: "Sollten einzelne Bestimmungen dieser AGB unwirksam sein oder werden, bleiben die übrigen Bestimmungen davon unberührt.",
+        },
+      ],
+    },
+  ],
+};
